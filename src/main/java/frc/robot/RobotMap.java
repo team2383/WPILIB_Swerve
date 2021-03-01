0af229fdec52d93d987535910f10c800b5a649bb
stan173 <54913391+stan173@users.noreply.github.com>
2021-03-01 16:39:31 -0600

    more initialization

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,4 +5,20 @@
 package frc.robot;
 
 /** Add your docs here. */
-public class RobotMap {}
+public class RobotMap {
+    public static int frontRightDrivePort = 0;
+    public static int frontLeftDrivePort = 1;
+    public static int backRightDrivePort = 2;
+    public static int backLeftDrivePort = 3;
+
+    public static int frontRightTurnPort = 4;
+    public static int frontLeftTurnPort = 5;
+    public static int backRightTurnPort = 6;
+    public static int backLeftTurnPort = 7;
+
+    //space between wheels in meters (i think)
+    public static double trackWidth = 1.0;
+    //space between consecutive wheels in meters (i think)
+    public static double trackLength = 1.0;
+
+}
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,16 +5,91 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+import java.util.Arrays;
 
 public class DriveSubsystem extends SubsystemBase {
+
+  //Defining Motor variables and kinematics
+  public WPI_TalonFX frontRightDrive, frontLeftDrive, backRightDrive, backLeftDrive;
+  public WPI_TalonSRX frontRightTurn, frontLeftTurn, backRightTurn, backLeftTurn;
+  SwerveDriveKinematics m_kinematics;
+
   /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {}
+  public DriveSubsystem() {
+    //Assigning variables to motor objects
+    frontRightDrive = new WPI_TalonFX(RobotMap.frontRightDrivePort);
+    frontLeftDrive = new WPI_TalonFX(RobotMap.frontLeftDrivePort);
+    backRightDrive = new WPI_TalonFX(RobotMap.backRightDrivePort);
+    backLeftDrive = new WPI_TalonFX(RobotMap.backLeftDrivePort);
+    
+    frontRightTurn = new WPI_TalonSRX(RobotMap.frontRightTurnPort);
+    frontLeftTurn = new WPI_TalonSRX(RobotMap.frontLeftTurnPort);
+    backRightTurn = new WPI_TalonSRX(RobotMap.backRightTurnPort);
+    backLeftTurn = new WPI_TalonSRX(RobotMap.backLeftTurnPort);
+
+    Translation2d m_frontLeftLocation = new Translation2d((RobotMap.trackWidth)/2, (RobotMap.trackLength)/2);
+    Translation2d m_frontRightLocation = new Translation2d((RobotMap.trackWidth)/2, -(RobotMap.trackLength)/2);
+    Translation2d m_backLeftLocation = new Translation2d(-(RobotMap.trackWidth)/2, (RobotMap.trackLength)/2);
+    Translation2d m_backRightLocation = new Translation2d(-(RobotMap.trackWidth)/2, -(RobotMap.trackLength)/2);
+
+    m_kinematics = new SwerveDriveKinematics(
+                m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+        );
+  }
+
+  public void configDriveControllers(){
+    frontRightDrive.setInverted(false);
+    frontLeftDrive.setInverted(false);
+    backRightDrive.setInverted(false);
+    backLeftDrive.setInverted(false);
+
+    frontRightDrive.setSensorPhase(true);
+    frontLeftDrive.setSensorPhase(true);
+    backRightDrive.setSensorPhase(true);
+    backLeftDrive.setSensorPhase(true);
+    
+    frontRightDrive.setNeutralMode(NeutralMode.Coast);
+    frontLeftDrive.setNeutralMode(NeutralMode.Coast);
+    backRightDrive.setNeutralMode(NeutralMode.Coast);
+    backLeftDrive.setNeutralMode(NeutralMode.Coast);
+    
+    frontRightDrive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+    frontLeftDrive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+    backRightDrive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+    backLeftDrive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+  }
+
+  public void configTurnControllers(){
+    frontRightTurn.setInverted(false);
+    frontLeftTurn.setInverted(false);
+    backRightTurn.setInverted(false);
+    backLeftTurn.setInverted(false);
+
+    frontRightTurn.setSensorPhase(true);
+    frontLeftTurn.setSensorPhase(true);
+    backRightTurn.setSensorPhase(true);
+    backLeftTurn.setSensorPhase(true);
+    
+    frontRightTurn.setNeutralMode(NeutralMode.Brake);
+    frontLeftTurn.setNeutralMode(NeutralMode.Brake);
+    backRightTurn.setNeutralMode(NeutralMode.Brake);
+    backLeftTurn.setNeutralMode(NeutralMode.Brake);
+    
+    frontRightTurn.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, 0, 30);
+    frontLeftTurn.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, 0, 30);
+    backRightTurn.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, 0, 30);
+    backLeftTurn.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, 0, 30);
+  }
 
   @Override
   public void periodic() {
